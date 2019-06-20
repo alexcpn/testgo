a8217a3c9c868480dde04be62b2b249348918cf4
AlexPunnen <alex.punnen@nokia.com>
2019-06-20 11:41:20 +0530

    test

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1,5 +1,18 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+
+	"github.com/gocql/gocql"
+)
+
 func main() {
- fmt.Println("Hello, World ")
+	fmt.Println("Hello, World -Test cassandra")
+
+	cluster := gocql.NewCluster("mycassandra")
+	cluster.Keyspace = "sm"
+	cluster.Consistency = gocql.Quorum
+	session, _ := cluster.CreateSession()
+	defer session.Close()
+	fmt.Println("Connected closed")
 }
