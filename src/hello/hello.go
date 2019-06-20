b52650b8396b2054b7f02cd98cbcb24ced317f7c
AlexPunnen <alex.punnen@nokia.com>
2019-06-20 12:12:46 +0530

    read write

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello, World -Test cassandra")
+	fmt.Println("Hello, World -Test  Cassandra")
 
 	cluster := gocql.NewCluster("mycassandra")
 	cluster.Keyspace = "sm"
@@ -22,13 +22,25 @@ func main() {
 	var imsi string
 	var msisdn string
 	var opc string
+	
+	//Add an etry to the table
+
+	if err := session.Query("INSERT INTO  sm.sim_inventory (imsi, msisdn, opc) VALUES (?, ?, ?)",
+	"132312321", "7777777", "adsdasdas1").Exec(); err != nil {
+		log.Fatalf("Could not Insert to cassandra : %v", err)
+	}
+
 	// Fetch multiple rows and run process over them
     iter := session.Query("SELECT imsi, msisdn,opc FROM sm.sim_inventory").Iter()
     for iter.Scan(&imsi, &msisdn, &opc) {
         log.Printf("Iter imsi: %v", imsi)
 		log.Printf("Iter msisdn: %v", msisdn)
 		log.Printf("Iter opc: %v", opc)
-    }
+	}
+	if err := iter.Close(); err != nil {
+		log.Fatalf("Could not Query table : %v", err)
+	}
+	
  
 	
 	fmt.Println("Connected closed")
